654fc724017d6e1d75165f6454be0cf96ba18c42
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-21 15:18:28 +0300

    created model 'Course'

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=50, verbose_name='название курса')
+    preview = models.ImageField(upload_to='courses/', verbose_name='превью курса', **NULLABLE)
+    description = models.TextField(verbose_name='описание курса')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
