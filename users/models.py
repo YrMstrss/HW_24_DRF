2d469afb5d8adfdc2aebda927cd06dd0c54df4e4
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-21 15:11:10 +0300

    changed 'User' model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+NULLABLE = {'null': True, 'blank': True}
 
 
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=30, verbose_name='Номер телефона')
+    city = models.CharField(max_length=50, verbose_name='Город')
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватарка', **NULLABLE)
+
+    email = models.EmailField(unique=True, verbose_name='email')
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
