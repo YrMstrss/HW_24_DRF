3cca83bb0e8b4818281ae5cce3d9493eb9281e42
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-21 15:22:52 +0300

    created model 'Lesson'

--- lessons/models.py
+++ lessons/models.py
@@ -14,3 +14,17 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=50, verbose_name='название урока')
+    preview = models.ImageField(upload_to='lessons/', verbose_name='превью урока', **NULLABLE)
+    description = models.TextField(verbose_name='описание урока')
+    video_link = models.URLField(verbose_name='ссылка на видео')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
