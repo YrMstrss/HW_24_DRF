1327026a5f0fa96f9897dd41ee1d991ad4ccc8c9
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-22 14:28:47 +0300

    created url for 'Lesson' create

--- lessons/views.py
+++ lessons/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.generics import CreateAPIView
 
 from lessons.models import Course
-from lessons.serializers import CourseSerializer
+from lessons.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
