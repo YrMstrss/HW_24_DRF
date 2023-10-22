0f7e7642abe53536b688dcebc89782c4f29788fd
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-22 14:33:44 +0300

    created url for 'Lesson' list and single view

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from lessons.apps import LessonsConfig
 from rest_framework.routers import DefaultRouter
 
-from lessons.views import CourseViewSet, LessonCreateAPIView
+from lessons.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView
 
 app_name = LessonsConfig.name
 
@@ -12,4 +12,6 @@ router.register(r'course', CourseViewSet, basename='courses')
 
 urlpatterns = [
     path('lesson/create', LessonCreateAPIView.as_view(), name='create-lesson'),
+    path('lesson/', LessonListAPIView.as_view(), name='list-lesson'),
+    path('lesson/<int:pk>', LessonRetrieveAPIView.as_view(), name='view-lesson'),
 ] + router.urls
