baa90c2c1a1f5fbe80d6a3347e2f1504a04fec24
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-22 18:56:52 +0300

    added filters

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 
 from payments.models import Payment
@@ -7,3 +8,7 @@ from payments.serializers import PaymentSerializer
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('course', 'lesson', 'payment_method')
+
