6bcb97ee3804b249d2c4e7b170bb4647d9d87348
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-22 14:33:55 +0300

    created generics for 'Lesson' list and single view

--- lessons/views.py
+++ lessons/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets, generics
-from rest_framework.generics import CreateAPIView
 
-from lessons.models import Course
+from lessons.models import Course, Lesson
 from lessons.serializers import CourseSerializer, LessonSerializer
 
 
@@ -10,5 +9,15 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
 
 
-class LessonCreateAPIView(CreateAPIView):
+class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
