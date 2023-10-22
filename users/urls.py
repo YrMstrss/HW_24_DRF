8afbe0df823010a5e193095c29b65e39d5e402bd
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-22 19:18:35 +0300

    added User retrieve generic

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView
+from users.views import UserUpdateAPIView, UserRetrieveAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('user/edit/<int:pk>', UserUpdateAPIView.as_view(), name='edit-user'),
+    path('user/<int:pk>', UserRetrieveAPIView.as_view(), name='view-user'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,11 @@ from users.models import User
 from users.serializers import UserSerializer
 
 
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
