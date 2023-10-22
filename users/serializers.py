fb909aacca529592d3e2a8635bd1d991349e320a
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-22 19:18:58 +0300

    added payments to User

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from payments.serializers import PaymentSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(source='payment_set', many=True)
+
     class Meta:
         model = User
         fields = '__all__'
