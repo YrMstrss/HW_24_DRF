0743a4ff36fe3541c04aee283cd1b5f510e07da9
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-22 14:35:18 +0300

    created url for 'Lesson' update

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from lessons.apps import LessonsConfig
 from rest_framework.routers import DefaultRouter
 
-from lessons.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView
+from lessons.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView
 
 app_name = LessonsConfig.name
 
@@ -14,4 +15,5 @@ urlpatterns = [
     path('lesson/create', LessonCreateAPIView.as_view(), name='create-lesson'),
     path('lesson/', LessonListAPIView.as_view(), name='list-lesson'),
     path('lesson/<int:pk>', LessonRetrieveAPIView.as_view(), name='view-lesson'),
+    path('lesson/edit/<int:pk>', LessonUpdateAPIView.as_view(), name='edit-lesson'),
 ] + router.urls
