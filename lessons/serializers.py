be18a456810d4ec73434e6d8614efcdea9acafd0
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-25 18:26:49 +0300

    added validator to 'LessonSerializer'

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
 from lessons.models import Course, Lesson
+from lessons.validators import VideoLinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [VideoLinkValidator(field='video_link')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
