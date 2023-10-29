8db78d854e22afadd76cf4fe69ad04a9ee1442c0
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-29 15:10:10 +0300

    added 'Subscription' to admin page

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from lessons.models import Lesson, Course
+from lessons.models import Lesson, Course, Subscription
 
 admin.site.register(Lesson)
 
 admin.site.register(Course)
+
+admin.site.register(Subscription)
