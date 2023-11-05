b5e75b9e487f9fa6caf41199502c5a0e5bd7776c
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-05 18:05:12 +0300

    error correction

--- lessons/permissions.py
+++ lessons/permissions.py
@@ -5,15 +5,12 @@ from lessons.models import Subscription
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-
         return request.user == obj.owner
 
 
 class IsManager(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.groups.filter(name='manager'):
-            return True
-        return False
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='manager').exists()
 
 
 class IsSubscriber(BasePermission):
@@ -22,18 +19,10 @@ class IsSubscriber(BasePermission):
 
 
 class IsLessonSubscribed(BasePermission):
-    def has_object_permission(self, request, view, obj):
-
-        subscription = Subscription.objects.filter(user=request.user, course=obj.course)
-        if subscription:
-            return True
-        return False
+    def has_permission(self, request, view):
+        return Subscription.objects.filter(user=request.user).exists()
 
 
 class IsCourseSubscribed(BasePermission):
-    def has_object_permission(self, request, view, obj):
-
-        subscription = Subscription.objects.filter(user=request.user, course=view.get_object())
-        if subscription:
-            return True
-        return False
+    def has_permission(self, request, view):
+        return Subscription.objects.filter(user=request.user,).exists()
