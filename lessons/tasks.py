b8bfaea9147054844a168033b0a60a950da5f37d
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-11-08 18:44:52 +0300

    created task sending message about updating course

---
+++ lessons/tasks.py
@@ -0,0 +1,42 @@
+from celery import shared_task
+from django.conf import settings
+from django.core.mail import send_mail
+
+from lessons.models import Lesson, Subscription, Course
+
+
+@shared_task
+def send_message(pk, obj: str, method: str):
+    if obj == 'lesson':
+        instance = Lesson.objects.filter(pk=pk)
+        course = instance.course
+        subscription = Subscription.objects.filter(course=course)
+        users = [user for user in subscription.users.email]
+        if method == 'create':
+            send_mail(
+                subject=f'Изменения в курсе "{course.title}"',
+                message=f'В курс "{course.title}" добавлен новый урок "{instance.title}"',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=users,
+                fail_silently=False
+            )
+        elif method == 'update':
+            send_mail(
+                subject=f'Изменения в курсе "{course.title}"',
+                message=f'В урок "{instance.title}" курса "{course.title}" были внесены изменения',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=users,
+                fail_silently=False
+            )
+    else:
+        instance = Course.objects.filter(pk=pk)
+        subscription = Subscription.objects.filter(course=instance)
+        users = [user for user in subscription.users.email]
+        if method == 'update':
+            send_mail(
+                subject=f'Изменения в курсе "{instance.title}"',
+                message=f'В курс "{instance.title}" были внесены изменения',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=users,
+                fail_silently=False
+            )
